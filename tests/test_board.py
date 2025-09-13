ab49c1859d8049cf2225200b4325eb9eeb3d8770
Santiago <saar.martinez@alumno.um.edu.ar>
2025-09-13 19:31:22 -0300

    Clase backgammon

--- tests/test_board.py
+++ tests/test_board.py
@@ -35,44 +35,46 @@ class TestTablero(unittest.TestCase):
         self.assertEqual(self.tab.sacar_checker(23), "negro")
 
     def test_mover_checker(self):
-        self.assertEqual(self.tab.mover_checker(0,3), "blanco")
-        self.assertEqual(self.tab.mover_checker(5,7), "negro")
-        self.assertEqual(self.tab.mover_checker(11,12), "blanco")
-        self.assertEqual(self.tab.mover_checker(17,19), "blanco")
-        self.assertEqual(self.tab.mover_checker(19,23), "blanco")
+        self.assertEqual(self.tab.mover_checker(0,3,"blanco"), "blanco")
+        self.assertEqual(self.tab.mover_checker(5,7,"negro"), "negro")
+        self.assertEqual(self.tab.mover_checker(11,12,"blanco"), "blanco")
+        self.assertEqual(self.tab.mover_checker(17,19,"blanco"), "blanco")
+        self.assertEqual(self.tab.mover_checker(19,23,"blanco"), "blanco")
     
     def test_validar_movimiento(self):
         jugador_blanco = Jugador("Santi","blanco")
         jugador_negro = Jugador("Gonzalo","negro")
         self.assertEqual(self.tab.movimiento_valido(0,1, jugador_blanco),True)
-        self.assertEqual(self.tab.movimiento_valido(0,2, jugador_blanco), True)
-        self.assertEqual(self.tab.movimiento_valido(0,5, jugador_negro), True)
+        self.assertFalse(self.tab.movimiento_valido(1,5 , jugador_blanco))
+        self.assertTrue(self.tab.movimiento_valido(0,1,jugador_blanco))
+        self.tab.__contenedor__[5] = ["negro"]
+        self.tab.__contenedor__[1] = []
+        self.assertTrue(self.tab.movimiento_valido(0, 1, jugador_blanco))
 
     def test_movimiento_posicion_fuera_rango(self):
         jugador = Jugador("Sanchi", "blanco")
-
         resultado = self.tab.movimiento_valido(-1, 5 , jugador)
         self.assertFalse(resultado)
-
         resultado = self.tab.movimiento_valido(0,26, jugador)
         self.assertFalse(resultado)
-
         resultado = self.tab.movimiento_valido(55, 5 , jugador)
         self.assertFalse(resultado)
 
     def test_sacar_checker_posicion_vacia(self):
         resultado = self.tab.sacar_checker(1)
         self.assertIsNone(resultado)
-
         resultado = self.tab.sacar_checker(3)
         self.assertIsNone(resultado)
     
     def test_mover_checker_especial(self):
-        resultado = self.tab.mover_checker(-1,5)
+        resultado = self.tab.mover_checker(-1,5,"blanco")
+        self.assertIsNone(resultado)
+        resultado = self.tab.mover_checker(1,2,"negro")
         self.assertIsNone(resultado)
 
-        resultado = self.tab.mover_checker(1,2)
-        self.assertIsNone(resultado)
+    def test_almacenar_ficha(self):
+        resultado = self.tab.almacenamiento("blanco")
+        self.assertEqual(resultado, {"blanco": 1, "negras": 0})
 
 if __name__ == "__main__":
     unittest.main()
