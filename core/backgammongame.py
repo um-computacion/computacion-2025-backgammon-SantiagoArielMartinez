57785a76e1cc96097f748916c386625e8d238923
Santiago <saar.martinez@alumno.um.edu.ar>
2025-09-16 20:45:27 -0300

    Clase backgammon

--- core/backgammongame.py
+++ core/backgammongame.py
@@ -1,12 +1,25 @@
 from core.player import Jugador
+from core.board import Tablero
+from core.dice import Dados
+from core.checkers import Ficha
 class BackgammonGame:
     def __init__(self, jugador1,jugador2):
         self.__jugador1__ = Jugador(jugador1, "Negras")
         self.__jugador2__ = Jugador(jugador2, "blancas")
+        self.__tablero__ = Tablero()
+        self.__dados__ = Dados()
 
     def get_jugador1(self):
         return self.__jugador1__
     
     def get_jugador2(self):
         return self.__jugador2__
+    
+    def get_tablero(self):
+        return self.__tablero__
+    
+    def get_dados(self):
+        self.__dados__.tirar_dado()
+        return self.__dados__
+    
     
